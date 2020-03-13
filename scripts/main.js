b391b8a43439e0be6a4ca74dfbccc2c11da97436
splitsplitsplit <2857224329@qq.com>
2020-03-13 17:08:58 +0800

    JS1

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,88 @@
+document.getElementById('cn').onclick = () => {
+    document.title = '笑话生成器';
+    document.getElementById('lbl-customname').textContent = '请输入自定义的名字：';
+    document.getElementById('lbl-cn').textContent = '中国';
+    document.getElementById('lbl-us').textContent = '美国';
+    document.getElementById('lbl-uk').textContent = '英国';
+    document.getElementById('customname').placeholder = '李雷';
+    document.querySelector('.randomize').textContent = '随机生成笑话';
+};
 
+document.getElementById('us').onclick =
+    document.getElementById('uk').onclick = () => {
+        document.title = 'Silly story generator';
+        document.getElementById('lbl-customname').textContent = 'Enter custom name:';
+        document.getElementById('lbl-cn').textContent = 'CN';
+        document.getElementById('lbl-us').textContent = 'US';
+        document.getElementById('lbl-uk').textContent = 'UK';
+        document.getElementById('customname').placeholder = 'Bob';
+        document.querySelector('.randomize').textContent = 'Generate random story';
+    };
 
-let myImage=document.querySelector("img");
-myImage.onclick=function(){
-    let mySrc=myImage.getAttribute("src");
-    if(mySrc==="images/2.jpg") myImage.setAttribute("src","images/1.jpg");
-    else myImage.setAttribute("src","images/2.jpg");
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array) {
+    return array[Math.floor(Math.random() * array.length)];
 }
 
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-function setUserName(){
-    let myName=prompt("请输入你的名字");
-    if(!myName||myName===null) setUserName();
-   else{ 
-   localStorage.setItem("name",myName);
-   myHeading.textContent="欢迎你,"+myName;}
-}
-if(!localStorage.getItem("name")) setUserName();
-else {let StoredName=localStorage.getItem("name");
-      myHeading.textContent="欢迎你,"+StoredName;}
+const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const insertY = ["the soup kitchen", "Disneyland", "the White House"];
+const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+const storyTextCn = '外边有34度，于是:inserta:出去遛弯。当走到:insertb:时他们被眼前的景象惊呆了，然后就:insertc:。李雷全程目睹但他并没有慌，因为:inserta:是一个270斤的胖子，天气又辣么热。';
+const insertA = ['怪兽威利', '大老爹', '圣诞老人'];
+const insertB = ['救助站', '迪士尼乐园', '白宫'];
+const insertC = ['自燃了', '在人行道化成了一坨泥', '变成一条鼻涕虫爬走了'];
 
-myButton.onclick=function(){
-    setUserName();
+randomize.addEventListener('click', result);
+
+function result() {
+    let newStory, xItem, yItem, zItem, name;
+    if (document.getElementById("cn").checked) {
+        newStory = storyTextCn;
+        xItem = randomValueFromArray(insertA);
+        yItem = randomValueFromArray(insertB);
+        zItem = randomValueFromArray(insertC);
+        newStory = newStory.replace(":inserta:", xItem);
+        newStory = newStory.replace(":inserta:", xItem);
+        newStory = newStory.replace(":insertb:", yItem);
+        newStory = newStory.replace(":insertc:", zItem);
+        if (customName.value !== "") {
+            name = customName.value;
+            newStory = newStory.replace("李雷", name);
+        }
+    } else {
+        newStory = storyText;
+        xItem = randomValueFromArray(insertX);
+        yItem = randomValueFromArray(insertY);
+        zItem = randomValueFromArray(insertZ);
+        newStory = newStory.replace(":insertx:", xItem);
+        newStory = newStory.replace(":insertx:", xItem);
+        newStory = newStory.replace(":inserty:", yItem);
+        newStory = newStory.replace(":insertz:", zItem);
+
+        if (customName.value !== '') {
+            name = customName.value;
+            newStory=newStory.replace("Bob", name);
+
+        }
+
+        if (document.getElementById("uk").checked) {
+            const weight = Math.round(300 / 14);
+            const temperature = Math.round(5 * (94 - 32) / 9);
+            newStory = newStory.replace("94 fahrenheit", temperature+" centigrade");
+            newStory = newStory.replace("300 pounds", weight+" stone");
+
+        }
+    }
+
+    story.textContent = newStory;
+    story.style.visibility = 'visible';
+
+
+
+
+
 }
