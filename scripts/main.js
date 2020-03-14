72ddcdf4f5a68b3074d0550539292bd151ceee81
splitsplitsplit <2857224329@qq.com>
2020-03-14 17:16:09 +0800

    ball

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,94 @@
-function qS(S){
-    return document.querySelector(S);
-}
-const displayedImage=qS(".displayed-img");
-const thumbBar=qS(".thumb-bar");
-const btn=qS("button");
-const overlay=qS(".overlay");
 
-for(let i=1;i<6;i++){
-   const newImage=document.createElement("img");
-   newImage.setAttribute("src","images/"+i+".jpg");
-   thumbBar.appendChild(newImage);
-   newImage.onclick=function(e){
-       const imgSrc=e.target.getAttribute("src");
-       displayedImage.setAttribute("src",imgSrc);
-   }
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+
+function random(min,max) {
+  return Math.floor(Math.random()*(max-min)) + min;
 }
 
-btn.onclick=function(){
-    const btnClass=btn.getAttribute("class");
-    if(btnClass==="dark"){
-        btn.setAttribute("class","light");
-        btn.textContent="变亮";
-        overlay.style.backgroundColor="rgba(0,0,0,0.5)";
-    }else{
-        btn.setAttribute("class","dark");
-        btn.textContent="变暗";
-        overlay.style.backgroundColor="rgba(0,0,0,0)";
+
+function randomColor() {
+  return 'rgb(' +
+         random(0, 255) + ', ' +
+         random(0, 255) + ', ' +
+         random(0, 255) + ')';
+}
+
+function Ball(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+  Ball.prototype.draw=function(){
+      ctx.beginPath();
+      ctx.fillStyle=this.color;
+      ctx.arc(this.x,this.y,this.size,0,2*Math.PI);
+      ctx.fill();
+  }
+  Ball.prototype.update=function(){
+      if((this.x+this.size)>=width){
+          this.velX=-this.velX;
+      }if((this.x-this.size)<=0){
+          this.velX=-this.velX;
+      }if((this.y+this.size)>=height){
+          this.velY=-this.velY;
+      }if((this.y-this.size)<=0){
+          this.velY=-this.velY;
+      }
+      this.x += this.velX;
+      this.y += this.velY;
+  }
+  Ball.prototype.collisionDetect = function() {
+    for (var j = 0; j < balls.length; j++) {
+      if (!(this === balls[j])) {
+        var dx = this.x - balls[j].x;
+        var dy = this.y - balls[j].y;
+        var distance = Math.sqrt(dx * dx + dy * dy);
+  
+        if (distance < this.size + balls[j].size) {
+          balls[j].color = this.color = randomColor();
+          balls[j].velX=-balls[j].velX;
+          balls[j].velY=-balls[j].velY;
+          this.velX=-this.velX;
+          this.velY=-this.velY;
+        }
+      }
     }
-}
+  }
+
+
+  var balls = [];
+  function loop() {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+    ctx.fillRect(0, 0, width, height);
+  
+    while (balls.length < 25) {
+      var ball = new Ball(
+        random(0,width),
+        random(0,height),
+        random(-7,7),
+        random(-7,7),
+        randomColor(),
+        random(10,20)
+      );
+      balls.push(ball);
+    }
+  
+    for (var i = 0; i < balls.length; i++) {
+      balls[i].draw();
+      balls[i].update();
+      balls[i].collisionDetect();
+    }
+  
+    requestAnimationFrame(loop);
+  }
+  loop();
+  
